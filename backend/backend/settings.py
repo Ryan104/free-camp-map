5d909b3f71fd72fa551cfa1a6dec83b95a4ceab6
Ryan <ryan.d.elliott@gmail.com>
2017-11-29 13:48:24 -0700

    Configure postgres

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'campsites',
+        'USER': 'dbuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
