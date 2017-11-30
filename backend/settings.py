5c6031c4de72b6b26e1446ba982b504096e87e64
Ryan <ryan.d.elliott@gmail.com>
2017-11-29 19:01:10 -0700

    Heroku config

--- backend/settings.py
+++ backend/settings.py
@@ -128,6 +128,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # React frontend
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR,'static'),
 )
