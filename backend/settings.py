fa3141484f118f86ade2b200cebc6e3bf9cfee26
Ryan <ryan.d.elliott@gmail.com>
2017-11-29 19:58:38 -0700

    Heroku config

--- backend/settings.py
+++ backend/settings.py
@@ -125,11 +125,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+
 
 # React frontend
-#PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-#STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-#STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR,'static'),
-# )
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
