9f3180c3c5e6f663b5946ad84fcc964e0a25afa5
Ryan <ryan.d.elliott@gmail.com>
2017-11-30 16:56:00 -0700

    Add Header bar with material ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import GoogleMap from 'google-map-react';
-//import MapContainer from './components/MapContainer'
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import FlatButton from 'material-ui/FlatButton';
 import Marker from './components/Marker'
 
 require('dotenv').config()
@@ -8,29 +11,38 @@ require('dotenv').config()
 class App extends Component {
   render() {
     return (
-      <div>
-        <header>MAP</header>
-        <div >
-          <GoogleMap style={styles.mapContainer}
-            // bootstrapURLKeys={{
-            //   key: process.env.REACT_APP_MAP_KEY,
-            //   language: 'en'
-            // }}
-            defaultCenter={{lat: 59.95, lng: 30.33}}
-            defaultZoom={11}
-          >
-            <Marker lat={59.95} lng={30.33} text={"Hello Map!"}  />
-          </GoogleMap>
+      <MuiThemeProvider>
+        <div>
+          <AppBar 
+            title="Camp Free"
+            iconElementRight={<FlatButton label="Login" />} 
+          />
+          <div style={styles.mapContainer}>
+            <GoogleMap style={styles.mapComponent}
+              // bootstrapURLKeys={{
+              //   key: process.env.REACT_APP_MAP_KEY,
+              //   language: 'en'
+              // }}
+              defaultCenter={{lat: 59.95, lng: 30.33}}
+              defaultZoom={11}
+            >
+              <Marker lat={59.95} lng={30.33} text={"Hello Map!"}  />
+            </GoogleMap>
+          </div>
         </div>
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
 
 const styles = {
   mapContainer: {
-      minHeight: '300px',
-      minWidth: '300px'
+
+  },
+  mapComponent: {
+      height: '100%',
+      minWidth: '100%',
+      position: 'absolute'
   }
 }
 
