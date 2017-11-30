8304cee2198a4536845abfb81375b8f498db8275
Ryan <ryan.d.elliott@gmail.com>
2017-11-29 21:47:05 -0700

    fix merge

--- backend/settings.py
+++ backend/settings.py
@@ -90,9 +90,6 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -139,7 +136,7 @@ STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 db_from_env = dj_database_url.config(conn_max_age=500)
