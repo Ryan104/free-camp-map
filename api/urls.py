e541c83ebd89530c4578243cf511d70f7985da32
Ryan <ryan.d.elliott@gmail.com>
2017-12-02 11:49:09 -0700

    Add api/auth url pattern

--- api/urls.py
+++ api/urls.py
@@ -3,8 +3,8 @@ Url patterns for the api app
 From root /api/
 '''
 from django.conf.urls import url
-from . import views
+from rest_framework.authtoken import views as drf_views
 
 urlpatterns = [
-
+    url(r'^auth$', drf_views.obtain_auth_token, name='auth'),
 ]
