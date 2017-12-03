dd2903e8860ff1bd61e5f40a74d953cf5a5e1b0a
Ryan <ryan.d.elliott@gmail.com>
2017-12-02 17:57:07 -0700

    Add popover to markers with site name

--- frontend/src/components/Marker.js
+++ frontend/src/components/Marker.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 // import shouldPureComponentUpdate from 'react-pure-render/function';
+import { Popover } from 'material-ui'
 import MapsPlace from 'material-ui/svg-icons/maps/place'
-import { pinkA200 } from 'material-ui/styles/colors'
+import { pinkA200, pinkA400 } from 'material-ui/styles/colors'
 
 export default class Marker extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            open: false
+        }
+    }
+
+    handleTouchTap = (event) => {
+        // This prevents ghost click.
+        event.preventDefault();
+    
+        this.setState({
+          open: true,
+          anchorEl: event.currentTarget,
+        });
+    };
+
+    handleRequestClose = () => {
+        this.setState({
+            open: false,
+        });
+    };
+
     static propTypes = {
         name: PropTypes.string
     }
@@ -13,24 +37,31 @@ export default class Marker extends Component {
 
     // shouldComponentUpdate = shouldPureComponentUpdate
 
-    styles = {
-        markerContainer: {
-            position: 'absolute',
-            backgroundColor: 'white',
-            border: '1px solid black',
-            borderRadius: 10,
-            padding: 4,
-            width: '100px'
-        },
-        marker: {
-            color: pinkA200
-        }
-    }
-
     render(){
+        let styles = {
+            marker: {
+                color: this.state.open ? pinkA400 : pinkA200,
+                width: this.state.open ? 60 : 30,
+                height: this.state.open ? 60 : 30,
+            },
+            popover: {
+                padding: 10
+            }
+        }
+
         return (
-            <div data-id={this.props.id}>
-                <MapsPlace style={this.styles.marker} />
+            <div data-id={this.props.dataId}>
+                <MapsPlace style={styles.marker} onClick={this.handleTouchTap} />
+                <Popover
+                    open={this.state.open}
+                    anchorEl={this.state.anchorEl}
+                    anchorOrigin={{"horizontal":"left","vertical":"center"}}
+                    targetOrigin={{"horizontal":"right","vertical":"center"}}
+                    onRequestClose={this.handleRequestClose}
+                    style={styles.popover}
+                >
+                    {this.props.text}
+                </Popover>
             </div>
         )
     }
